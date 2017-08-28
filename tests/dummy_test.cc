eb45c209c80539e37e1bcf3a8b2df07cd3c1f827
Robert Peters <robert.peters.dd@gmail.com>
2017-08-28 15:43:39 +0200

    Add third party test framework catch

---
+++ tests/dummy_test.cc
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+
+unsigned int fact(unsigned int number)
+{
+	return number <= 1 ? number : fact(number-1)*number;
+}
+
+TEST_CASE("factorials are computed", "[factorial]" )
+{
+    REQUIRE(fact(1)  ==       1);
+    REQUIRE(fact(2)  ==       2);
+    REQUIRE(fact(3)  ==       6);
+    REQUIRE(fact(10) == 3628800);
+}
+
