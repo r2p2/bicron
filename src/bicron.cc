155b1a0f7817d01daab6937a763d6aea2674b04a
Robert Peters <robert.peters.dd@gmail.com>
2017-08-31 13:30:07 +0200

    Generate TimePoint based on current time

--- src/bicron.cc
+++ src/bicron.cc
@@ -5,6 +5,27 @@ void Bicron::update(Config cfg)
 	_config = std::move(cfg);
 }
 
+Bicron::result_t Bicron::status() const
+{
+	TimePoint tp;
+	time_t tt_now = time(0);
+	tm* now;
+
+	bool use_local_time = true;
+	if (use_local_time)
+		now = localtime(&tt_now);
+	else
+		now = gmtime(&tt_now);
+
+	tp.year(1900 + now->tm_year);
+	tp.month(1 + now->tm_mon);
+	tp.day(now->tm_mday);
+	tp.hour(now->tm_hour);
+	tp.minute(now->tm_min);
+
+	return status(tp);
+}
+
 Bicron::result_t Bicron::status(TimePoint const& tp) const
 {
 	result_t result;
